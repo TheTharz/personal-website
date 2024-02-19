d64d41b05e6ab8f0a68000bcd0992ed472ca4f7e
Tharindu Jayawardhana <tharinduimalka915@gmail.com>
2024-02-19 13:14:03 +0530

    Update base URL and add deploy script

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/personal_website_portfolio/',
+  base: '/personal-website/',
   plugins: [react()],
 });
