51ef4f296910b99bba77aacb4076ffcda7d16556
Tharindu Jayawardhana <tharinduimalka915@gmail.com>
2025-02-23 09:34:28 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/personal-website/',
+  base: '/portfolio/',
   // base: '/',
   plugins: [react()],
 });
